09923eb1c05ecb3c6f94b3cf8478448b7a5b4528
masood <masood_a_t@yahoo.com>
2024-08-17 01:20:46 +0400

    Update LootManager.cs

--- Assets/Loot/LootManager.cs
+++ Assets/Loot/LootManager.cs
@@ -2,38 +2,61 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.EventSystems.EventTrigger;
 using Random = UnityEngine.Random;
 
 public class LootManager : MonoBehaviour
 {
     public List<Loot> lootList;
     public PlayerModel _playerModel;
-    //[SerializeField] private int lootCount;
     public float SpawnDistance;
     [SerializeField] private GameObject _player;
     [SerializeField] private float _delayForRespawn;
+    public List<ExtendedPool<Loot>> LootPools;
+
 
     public void Initialize(PlayerModel playerModel, GameObject player, EnemiesController enemiesController)
     {
         _playerModel = playerModel;
         _player = player;
-        GenerateNewRandomLoot();
         StartCoroutine(Spwner());
         enemiesController.OnEnemyKilled += SpawnLoot;
+
+
+
+        LootPools = new List<ExtendedPool<Loot>>();
+        for (int i = 0; i < lootList.Count; i++)
+        {
+            int currentIndex = i;
+            LootPools.Add(new ExtendedPool<Loot>(
+            createFunc: () => { return Instantiate(lootList[currentIndex]); },//create
+            loot => loot.gameObject.SetActive(true),//onget
+            loot => loot.gameObject.SetActive(false),//onrelease
+            loot => Destroy(loot.gameObject)//ondestroy
+            ));
+        }
+
+        GenerateNewRandomLoot();
+
     }
     public void SpawnLoot(Vector3 position)
     {
-        int r = Random.Range(0, lootList.Count);
-        var l = Instantiate(lootList[r]);
+        int r = Random.Range(0, LootPools.Count);
+        var l = LootPools[r].Get();//Instantiate(lootList[r]);
         l.Initialize(this);
         l.transform.position = position;
-        //lootCount++;
     }
 
     public void CollectLoot(Action<PlayerModel> ApplyEffect)
     {
         ApplyEffect(_playerModel);
-        //lootCount--;
+    }
+
+    public void ReleaseLoot(Loot obj)
+    {
+        foreach (var pool in LootPools)
+            if (pool.IsInList(obj))
+                pool.Release(obj);
     }
 
     private void GenerateNewRandomLoot()
@@ -45,7 +68,7 @@ public class LootManager : MonoBehaviour
 
     IEnumerator Spwner()
     {
-        while (true)
+        while (_playerModel.IsPlayerAlive())
         {
             yield return new WaitForSeconds(_delayForRespawn);
             GenerateNewRandomLoot();
