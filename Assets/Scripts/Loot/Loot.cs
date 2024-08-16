68aeefcec946e913bb89c6a2f1c752f713ee65de
masood <masood_a_t@yahoo.com>
2024-08-17 02:25:12 +0400

    Create Loot.cs

---
+++ Assets/Scripts/Loot/Loot.cs
@@ -0,0 +1,26 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+public abstract class Loot : MonoBehaviour
+{
+    [SerializeField] protected LootSO data;
+    [SerializeField] LootManager _lootManager;
+    public abstract void ApplyEffect(PlayerModel playerModel);
+    public void Initialize(LootManager lootManager)
+    {
+        _lootManager = lootManager;
+    }
+
+    protected void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            _lootManager.CollectLoot(ApplyEffect);
+            transform.DOMove(collision.transform.position, 0.25f).
+                OnComplete(() =>
+                {
+                    _lootManager.ReleaseLoot(this);
+                });
+        }
+    }
+}
