6ff97dc2a0cb503516cbbefed97a12647b7cde42
masood <masood_a_t@yahoo.com>
2024-08-17 02:25:06 +0400

    Update HealthLoot.cs

--- Assets/Scripts/Loot/HealthLoot.cs
+++ Assets/Scripts/Loot/HealthLoot.cs
@@ -1,29 +1,3 @@
-using DG.Tweening;
-using UnityEngine;
-
-public abstract class Loot : MonoBehaviour
-{
-    [SerializeField] protected LootSO data;
-    [SerializeField] LootManager _lootManager;
-    public abstract void ApplyEffect(PlayerModel playerModel);
-    public void Initialize(LootManager lootManager)
-    {
-        _lootManager = lootManager;
-    }
-
-    protected void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.CompareTag("Player"))
-        {
-            _lootManager.CollectLoot(ApplyEffect);
-            transform.DOMove(collision.transform.position, 0.25f).
-                OnComplete(() =>
-                {
-                    _lootManager.ReleaseLoot(this);
-                });
-        }
-    }
-}
 public class HealthLoot : Loot
 {
     public override void ApplyEffect(PlayerModel playerModel)
